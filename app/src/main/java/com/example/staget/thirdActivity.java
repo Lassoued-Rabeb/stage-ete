382148acdd0db8768e6ea76fdbcc4ba2925f07d7
Rabeb <lassouedrabeb11@gmail.com>
2024-07-21 00:27:13 +0200

    first commit

---
+++ app/src/main/java/com/example/staget/thirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.staget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
